cd151fe8d35732838d6afca690150bddcb45df4c
Rana X Adhikari <rxa254@users.noreply.github.com>
2025-04-24 21:52:40 -0400

    Create __init__.py

---
+++ quantum_staircase/vendor/__init__.py
@@ -0,0 +1,7 @@
+# quantum_staircase/vendor/__init__.py
+"""
+Light-weight “vendor” namespace for third-party single-file modules that we
+prefer not to add as PyPI runtime dependencies.  Anything placed here is
+considered internal to *quantum_staircase* and carries its original license
+header (see the individual files).
+"""
